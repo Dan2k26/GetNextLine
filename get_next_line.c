006f457a85ef5cc591ec154d991f2f210d9d5f11
Dan <daniellerma05218@gmail.com>
2021-09-03 15:44:34 +0200

    Imprime las lineas correctamente
    Errores de liberacion y falta la variable estatica para almacenar los caracteres sobrantes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dlerma-c <dlerma-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 14:09:52 by dlerma-c          #+#    #+#             */
-/*   Updated: 2021/09/02 20:39:53 by dlerma-c         ###   ########.fr       */
+/*   Updated: 2021/09/03 14:53:51 by dlerma-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,15 @@ static char	*join_chars(char *buff, char *str)
 {
 	char	*concat;
 
-	concat = ft_strjoin(str, buff);
-	free(str);
-	str = ft_strdup(concat);
-	free(concat);
+	/*if (buff != '\0')
+	{*/
+		concat = ft_strjoin(str, buff);
+		free(str);
+		str = ft_strdup(concat);
+		free(concat);
+	/*}
+	else
+		str = ft_strdup("s");*/
 	return (str);
 }
 
@@ -35,21 +40,31 @@ static char	*join_chars(char *buff, char *str)
 */
 char	*get_next_line(int fd)
 {
-	char	buff[BUFFER_SIZE + 1];
-	char	*str;
-	char	*str_fnl;
+	char		buff[BUFFER_SIZE + 1];
+	char		*str;
+	char		*str_fnl;
+	//static char	*aux;
 	ssize_t	size;
 
-	size = read(fd, buff, BUFFER_SIZE);
-	buff[size] = '\0';
-	if (fd == 2 || size == 0)
+	if (fd < 0 || read(fd, buff, 0) == -1)
 		return (NULL);
+	size = read(fd, buff, BUFFER_SIZE);
+	if (size == 0)
+		return (NULL);
+	buff[size] = '\0';
 	str = ft_strjoin("", buff);
 	while (size > 0)
 	{
 		size = read(fd, buff, BUFFER_SIZE);
+		buff[size] = '\0';
 		if (buff[size - 1] == '\n' || size == 0)
-			return (str);
+		{
+			str = join_chars(buff, str);
+			str_fnl = str;
+			if (buff[0] != '\0')
+				free(str); //libera cuando no hay nada
+			return (str_fnl);
+		}
 		str = join_chars(buff, str);
 	}
 	str_fnl = str;
