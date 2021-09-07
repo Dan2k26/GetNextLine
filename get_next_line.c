8869abd39ecad0b6bb2aaf2ddf6d518a9ca58164
Dan <daniellerma05218@gmail.com>
2021-09-07 15:40:52 +0200

    Parches
    parches a buffer grande, rompe codigo. No genera segfault pero crea bucles infinitos

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dlerma-c <dlerma-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 14:09:52 by dlerma-c          #+#    #+#             */
-/*   Updated: 2021/09/06 22:08:17 by dlerma-c         ###   ########.fr       */
+/*   Updated: 2021/09/07 15:31:30 by dlerma-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,11 +66,11 @@ char	*get_next_line(int fd)
 	if (fd < 0 || read(fd, buff, 0) == -1)
 		return (NULL);
 	size = read(fd, buff, BUFFER_SIZE);
-	if (size == 0)
+	if (size == 0 && !temp)
 		return (NULL);
 	buff[size] = '\0';
 	str = conditions(temp);
-	while (size > 0)
+	while (size > 0) //si temp sigue existiendo, no entra
 	{
 		if (ft_strchr(buff, '\n') != NULL)
 		{
@@ -86,5 +86,16 @@ char	*get_next_line(int fd)
 		size = read(fd, buff, BUFFER_SIZE);
 		buff[size] = '\0';
 	}
+	//si temp existe y el fichero ha terminado de leer
+	if (buff[0] != '\0' && ((temp[0] != '\0') && size == 0))
+	{
+		aux = temp;
+		str = till_newline(aux);
+		temp = ft_strdup(ft_strchr(aux, '\n') + 1);
+		free(aux);
+		return (str);
+	}
+	else if (buff[0] != '\0' && (temp[0] == '\0' && size == 0))
+		return (NULL);
 	return (str);
 }
