4203d14683360909d33e20fefe0b94e256ff642e
Dan <daniellerma05218@gmail.com>
2021-09-15 19:23:45 +0200

    Nuevo planteamiento
    ver si hay algo en remainder y luego imprimir. Si hay un buffer grande esta llamando a leer todo el rato cuando tiene todo almacenado ya

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dlerma-c <dlerma-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 14:09:52 by dlerma-c          #+#    #+#             */
-/*   Updated: 2021/09/09 18:59:47 by dlerma-c         ###   ########.fr       */
+/*   Updated: 2021/09/15 19:20:41 by dlerma-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@ static char	*assign_chars(char *aux)
 {
 	int		i;
 	char	*str;
-	
+
 	i = 0;
-	//da los valores hasta el salto de linea
 	while (aux[i] != '\n' && aux[i] != '\0')
 		i++;
 	str = malloc(sizeof(char) * i + 1);
@@ -44,28 +43,17 @@ static char	*read_line(int size, int fd, char *buff, char **str)
 
 	while (size > 0)
 	{
-		//Concatenar la linea actual con la anterior
 		aux = *str;
 		*str = ft_strjoin(aux, buff);
 		free(aux);
-		//Comprueba el salto de linea
-		//Si hay salto de linea, salimos del bucle
 		if (ft_strchr(*str, '\n') != NULL)
 			break ;
-		//Si no sigue leyendo
 		size = read(fd, buff, BUFFER_SIZE);
 		buff[size] = '\0';
 	}
 	return (*str);
 }
 
-/*
-* buff -> string que acaba de leer del fichero
-* str -> linea que tiene que devolver
-* aux -> apoyo a str
-* temp -> lo que sobra despues de un salto de linea
-* size -> el numero de caracteres que ha leido (puede ser distinto a BUFFER_SIZE)
-*/
 char	*get_next_line(int fd)
 {
 	char		buff[BUFFER_SIZE + 1];
@@ -73,26 +61,29 @@ char	*get_next_line(int fd)
 	static char	remainder[BUFFER_SIZE + 1];
 	char		*aux;
 	ssize_t		size;
-//1- Comprobar errores de fichero
+
 	if (read(fd, buff, 0) == -1 || BUFFER_SIZE < 1)
 		return (NULL);
-//2- Leer y comprobar que no da error 
-	size = read(fd, buff, BUFFER_SIZE);
 	if (size == 0 && remainder[0] == '\0')
 		return (NULL);
+	if (ft_strchr((char *)remainder, '\n'))
+	{
+		str = ft_strdup((char *)remainder);
+		aux = str;
+		str = assign_chars(aux);
+		free(aux);
+		ft_memcpy((char *)remainder, ft_strchr(str, '\n') + 1,
+			ft_strlen(ft_strchr(str, '\n')));
+		return (str);
+	}
+	size = read(fd, buff, BUFFER_SIZE);
 	buff[size] = '\0';
-//3- La estatica ya esta inicializada
 	str = ft_strdup((char *)remainder);
 	clean_chr(remainder, sizeof(remainder));
-//4- Bucle de lectura
-	//paso por referencia str, que es la que puede tener un resultado u otro
 	str = read_line(size, fd, buff, &str);
-//5- Si tiene salto de linea guardar el temp lo sobrante
-	// LEAKS
 	if (ft_strchr(str, '\n') != NULL)
 		ft_memcpy((char *)remainder, ft_strchr(str, '\n') + 1,
 			ft_strlen(ft_strchr(str, '\n')));
-//6- se le asignan os caracteres antes del salto de linea
 	aux = str;
 	str = assign_chars(aux);
 	free(aux);
