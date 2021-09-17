cf8fd9c9bbba66249fb9ed9c2032baf8466d7ff9
Dan <daniellerma05218@gmail.com>
2021-09-17 21:18:06 +0200

    Leaks pero arreglao

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dlerma-c <dlerma-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 14:09:52 by dlerma-c          #+#    #+#             */
-/*   Updated: 2021/09/17 19:28:30 by dlerma-c         ###   ########.fr       */
+/*   Updated: 2021/09/17 21:17:52 by dlerma-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,9 @@ static char	*assign_chars(char *aux)
 	i = 0;
 	while (aux[i] != '\n' && aux[i] != '\0')
 		i++;
-	str = calloc(sizeof(char), i);
+	str = malloc(sizeof(char) * i + 1);
 	ft_memcpy(str, aux, i + 1);
+	str[i + 1] = '\0';
 	return (str);
 }
 
@@ -36,71 +37,48 @@ static void	clean_chr(void *s, size_t n)
 		str[i] = 0;
 }
 
-static char	*read_line(int fd, char *buff, char **str)
+static char	*read_line(int size, int fd, char *buff, char **str)
 {
 	char	*aux;
-	ssize_t	size;
 
-	size = read(fd, buff, BUFFER_SIZE);
-	if (size == 0 && buff == NULL)
-		return (NULL);
 	while (size > 0)
 	{
 		aux = *str;
 		*str = ft_strjoin(aux, buff);
-		free(aux);
+		
 		if (ft_strchr(*str, '\n') != NULL)
 			break ;
 		size = read(fd, buff, BUFFER_SIZE);
 		buff[size] = '\0';
 	}
+	free(aux);
 	return (*str);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buff;
+	char		buff[BUFFER_SIZE + 1];
 	char		*str;
+	static char	*remainder;
 	char		*aux;
+	ssize_t		size;
 
-	if (!buff)
-	{
-		buff = calloc(sizeof(char), BUFFER_SIZE + 1);
-		if (buff == NULL)
-			return (NULL);
-		buff[BUFFER_SIZE] = '\0';
-	}
 	if (read(fd, buff, 0) == -1 || BUFFER_SIZE < 1)
 		return (NULL);
-	if (ft_strchr(buff, '\n') != NULL)
-	{
-		str = assign_chars(buff);
-		buff = ft_strchr(buff, '\n') + 1;
-		return(str);
-	}
-	str = ft_strdup(buff);
-	str = read_line(fd, buff, &str);
-	if (str != NULL)
-	{
-		aux = str;
-		str = assign_chars(aux);
-		//free(aux);
-		buff = ft_strchr(buff, '\n') + 1;
-		return(str);
-	}
-
-
-
-	/*if (size == 0 && remainder[0] == '\0')
-		return (NULL);
+	size = read(fd, buff, BUFFER_SIZE);
+	if (size == 0 && remainder[0] == '\0')
+		return (NULL);	
 	buff[size] = '\0';
+	if (!remainder)
+		remainder = calloc(sizeof(char), BUFFER_SIZE);	
 	str = ft_strdup((char *)remainder);
 	clean_chr(remainder, sizeof(remainder));
+	str = read_line(size, fd, buff, &str);
 	if (ft_strchr(str, '\n') != NULL)
-		ft_memcpy((char *)remainder, ft_strchr(str, '\n') + 1,
-			ft_strlen(ft_strchr(str, '\n')));
+		remainder = ft_strdup(ft_strchr(str, '\n') + 1);
 	aux = str;
 	str = assign_chars(aux);
-	free(aux);*/
-	return (NULL);
+	free(aux);
+	//printf("------>  REMMAINDER: %s  <-------\n", remainder);
+	return (str);
 }
